4b28646c9eb3fc15d156e5013547753978877348
Кустова Мария Александровна <MKustova@ibs.root.ibscorp.ru>
2022-10-24 13:10:48 +0500

    Добавляет Type Guard

--- src/components/common/ErrorMessage/ErrorMessage.tsx
+++ src/components/common/ErrorMessage/ErrorMessage.tsx
@@ -9,15 +9,23 @@ interface ErrorMessageProps {
   message: FieldError | string;
 }
 
+// type Guard
+
+const isString = (message: FieldError | string): message is string => {
+  return typeof message === "string";
+};
+
 const ErrorMessage: FC<ErrorMessageProps> = ({ message }) => {
   const errorMessage = useMemo(() => {
-      if (typeof message === "string") {
-        return message;
-      }
-      return message.message;
+    if (isString(message)) {
+      return message;
+    }
+    return message.message;
   }, [message]);
 
-  return errorMessage ? <div className={s.ErrorMessage}>{capitalizeFirstLetter(errorMessage)}</div> : null;
+  return errorMessage ? (
+    <div className={s.ErrorMessage}>{capitalizeFirstLetter(errorMessage)}</div>
+  ) : null;
 };
 
 export default ErrorMessage;
